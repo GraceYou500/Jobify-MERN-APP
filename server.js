bfaf8ea101d7f2f616024d5ed4406f94d0256159
Grace You <graceyouya500@gmail.com>
2022-11-13 18:57:23 +1100

    setup backend && middleware

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+
+// middleware
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+app.get('/', (req, res) => {
+  throw new Error('Error');
+  res.send('Welcome!');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
