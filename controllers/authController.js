2292909585fb58ef097758bb0ee63b32cdc2389a
Grace You <graceyouya500@gmail.com>
2022-11-15 23:45:28 +1100

    custom status error and msg

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,20 @@
 import User from '../models/User.js';
+import { StatusCodes } from 'http-status-codes';
+
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
-  res.status(201).json({ user });
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new CustomAPIError('Please provide all values.');
+  }
+  const user = await User.create({ name, email, password });
+  res.status(StatusCodes.CREATED).json({ user });
   // next middleware will log the error to errorHandlerMiddleware. But with express-async-errors package, we don't need try catch and don't need next to pass the error, it will automatively pass to error handler middleware.
 };
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,31 @@
+import { StatusCodes } from 'http-status-codes';
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ msg: err });
+  console.log(err.message);
+
+  const defaultError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Sth went wrong, try again later',
+  };
+
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+    // console.log('defaultError.msg.............', defaultError.msg);
+  }
+
+  if (err.code && err.code === 11000) {
+    // 11000: duplicate error, meaning the field must be unique.
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique.`;
+
+    console.log('defaultError.msg.............', defaultError.msg);
+  }
+  // res.status(defaultError.statusCode).json({ msg: err });
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
