c3c73b55d038a784dd1051d32bb0a890e9ef0ce4
Grace You <graceyouya500@gmail.com>
2022-11-15 23:56:02 +1100

    extends error class

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,14 +4,27 @@ import { StatusCodes } from 'http-status-codes';
 class CustomAPIError extends Error {
   constructor(message) {
     super(message);
+  }
+}
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
     this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    throw new CustomAPIError('Please provide all values.');
+    throw new BadRequestError('Please provide all values.');
   }
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
