2be0e0561eb3423654b5ba1d9b1d8f877fe203c3
Grace You <graceyouya500@gmail.com>
2022-11-18 21:37:00 +1100

    nested page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import { Landing, Dashboard, Register, Error } from './pages';
+import { Landing, Register, Error } from './pages';
+import {
+  SharedLayout,
+  AllJobs,
+  Stats,
+  Profile,
+  AddJob,
+} from './pages/dashboard';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<SharedLayout />}>
+          {/* nested path don't need "/stats", without "/", because the nested route will relative to parent route automatively. */}
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='add-job' element={<AddJob />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,5 @@
-import Dashboard from './Dashboard';
 import Register from './Register';
 import Landing from './Landing';
 import Error from './Error';
 
-export { Dashboard, Landing, Error, Register };
+export { Landing, Error, Register };
