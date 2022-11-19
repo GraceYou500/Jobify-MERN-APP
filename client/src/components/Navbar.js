8ef8623f5ba1c259604d4dbb83f5843647d304e2
Grace You <graceyouya500@gmail.com>
2022-11-19 13:56:16 +1100

    navBar structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/Navbar';
-import { FaHome } from 'react-icons/fa';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import { Logo } from '../components';
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>NavBar</h4>
-      <FaHome />
+      <div className='nav-center'>
+        <button
+          type='button'
+          className='toggle-btn'
+          onClick={() => {
+            console.log('toggle sidebar');
+          }}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>Dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => {
+              console.log('Show/hide logout');
+            }}
+          >
+            <FaUserCircle />
+            John
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button
+              type='button'
+              className='dropdown-btn'
+              onClick={() => {
+                console.log('Logout user');
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
