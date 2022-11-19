50d3edc76dbb26e32fa4957781db41493cda867f
Grace You <graceyouya500@gmail.com>
2022-11-19 16:49:42 +1100

    toogle small sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,35 @@
-import React from 'react';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import links from '../utils/links';
+import Logo from './Logo';
+import { NavLink } from 'react-router-dom';
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>Small Sidebar</h4>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            {links.map((item) => (
+              <div className='nav-link'>
+                {item.icon} {item.text}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -3,19 +3,14 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import Wrapper from '../../assets/wrappers/SharedLayout';
 import { Navbar, SmallSidebar, BigSidebar } from '../../components';
-import { useAppContext } from '../../context/appContext';
 
 const SharedLayout = () => {
-  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
       <main className='dashboard'>
-        {showSidebar && (
-          <div>
-            <SmallSidebar />
-            <BigSidebar />
-          </div>
-        )}
+        <SmallSidebar />
+        <BigSidebar />
+
         <div>
           <Navbar />
           <div className='dashboard-page'>
