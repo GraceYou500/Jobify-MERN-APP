05b575f46b7f4232b079d3731acc4346ee7c5ca6
Grace You <graceyouya500@gmail.com>
2022-11-22 15:20:25 +1100

    create job model

---
+++ models/Job.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company'],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    jobType: {
+      type: String,
+      enum: ['part-time', 'full-time', 'remote', 'internship'],
+      default: 'full-time',
+    },
+    jobLocation: {
+      type: String,
+      required: true,
+      default: 'my city',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Job', JobSchema);
