ed211ad95ad75506678e4d128951b48f9c674906
Grace You <graceyouya500@gmail.com>
2022-11-30 19:22:03 +1100

    Chart container setup

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Wrapper from '../assets/wrappers/ChartsContainer';
+import { useAppContext } from '../context/appContext';
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
 
 const ChartsContainer = () => {
+  const [chartBar, setChartBar] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+
+  const toggleBarChart = () => {
+    setChartBar(!chartBar);
+  };
+
   return (
-    <div>
-      <h1>Charts Container</h1>
-    </div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={toggleBarChart}>
+        {chartBar ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {chartBar ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   );
 };
 
