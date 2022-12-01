337db480be667cad2053929a7337cc2a60e16602
Grace You <graceyouya500@gmail.com>
2022-12-01 16:30:34 +1100

    search by jobtype and status

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -38,7 +38,25 @@ const deleteJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  const { status, jobType, sort, search } = req.query;
+
+  const queryObject = { createdBy: req.user.userId };
+
+  // add stuff based on condition
+  if (status !== 'all') {
+    queryObject.status = status;
+  }
+
+  if (jobType !== 'all') {
+    queryObject.jobType = jobType;
+  }
+
+  // No await => get query promise and not the value
+  let result = Job.find(queryObject);
+
+  // chain sort conditions
+  const jobs = await result;
+
   res
     .status(StatusCodes.OK)
     .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
