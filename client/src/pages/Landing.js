2aaffb4a71726073475c341cf740079fa9444971
Grace You <graceyouya500@gmail.com>
2022-12-07 22:12:39 +1100

    landing page fix

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,33 +1,38 @@
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { Logo } from '../components'; // don't need to say the path, the index.js is the default one.
+import { useAppContext } from '../context/appContext';
 
 const Landing = () => {
+  const { user } = useAppContext();
   return (
-    <Wrapper>
-      <nav>
-        <Logo />
-      </nav>
-      <div className='container page'>
-        {/* Info */}
-        <div className='info'>
-          <h1>
-            job <span>tracking</span> app
-          </h1>
-          <p>
-            I'm baby intelligentsia tofu tumblr retro locavore. Paleo sartorial
-            pug sustainable marfa etsy trust fund. Bodega boys irony knausgaard
-            banh mi. Bicycle rights four dollar toast artisan jianbing
-            farm-to-table,
-          </p>
-          <Link className='btn btn-hero' to='/register'>
-            Login/Resister
-          </Link>
+    <>
+      {user && <Navigate to='/' />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className='container page'>
+          {/* Info */}
+          <div className='info'>
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              I'm baby intelligentsia tofu tumblr retro locavore. Paleo
+              sartorial pug sustainable marfa etsy trust fund. Bodega boys irony
+              knausgaard banh mi. Bicycle rights four dollar toast artisan
+              jianbing farm-to-table,
+            </p>
+            <Link className='btn btn-hero' to='/register'>
+              Login/Resister
+            </Link>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
         </div>
-        <img src={main} alt='job hunt' className='img main-img' />
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </>
   );
 };
 
